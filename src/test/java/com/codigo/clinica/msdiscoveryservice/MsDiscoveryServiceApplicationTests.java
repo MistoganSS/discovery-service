abd5a43d51d31446494e97e48a84ed58fd59e74f
Jhon Kevin <quispejhonn021@gmail.com>
2024-05-14 23:25:21 -0600

    discovery service

---
+++ src/test/java/com/codigo/clinica/msdiscoveryservice/MsDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.clinica.msdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
